70299c228f8cc81317e7e4786795754ca9b96796
Luis Fernando <luisfernando.mourag@gmail.com>
2024-07-14 08:08:12 -0300

    create query chamados

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,62 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { AuthContext } from '../../contexts/auth'
 
 import Header from '../../components/Header'
 import Title from '../../components/Title'
 import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+import { db } from '../../services/firebaseConnection'
+import { collection, getDocs, orderBy, limit, startAfter, query } from 'firebase/firestore'
+
 import './dashboard.css'
+
+const listRef = collection(db, "chamados")
 
 export default function Dashboard() {
   const { logout } = useContext(AuthContext);
 
-  async function handleLogout() {
-    await logout();
+  const [chamados, setChamados] = useState([])
+  const [loading, setLoading] = useState(true)
+  const { isEmpty, setIsEmpty } = useState(false)
+
+  useEffect(() => {
+    async function loadChamados() {
+      const q = query(listRef, orderBy('created', 'desc'), limit(5))
+
+      const querySnapshot = await getDocs(q)
+      await updateState(querySnapshot)
+
+      setLoading(false)
+    }
+    loadChamados()
+
+    return () => {
+
+    }
+  }, [])
+
+  async function updateState(querySnapshot) {
+    const isCollectionEmpty = querySnapshot.size === 0;
+
+    if (!isCollectionEmpty) {
+      let lista = []
+
+      querySnapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          assunto: doc.data().assunto,
+          cliente: doc.data().cliente,
+          clienteId: doc.data().clienteId,
+          created: doc.data().created,
+          status: doc.data().status,
+          complemento: doc.data().complemento
+        })
+      })
+
+      setChamados(chamados => [...chamados, ...lista])
+    } else {
+      setIsEmpty(true)
+    }
   }
 
   return (
@@ -23,58 +68,55 @@ export default function Dashboard() {
         </Title>
 
         <>
-          <Link to="/new" className="new">
-            <FiPlus color="#FFF" size={25} />
-            Novo Chamado
-          </Link>
+          {chamados.length === 0 ? (
+            <div className="container dashboard">
+              <span>Nenhum chamado encontrado...</span>
+              <Link to="/new" className="new">
+                <FiPlus color="#FFF" size={25} />
+                Novo Chamado
+              </Link>
+            </div>
+          ) : (
+            <>
+              <Link to="/new" className="new">
+                <FiPlus color="#FFF" size={25} />
+                Novo Chamado
+              </Link>
 
-          <table>
-            <thead>
-              <tr>
-                <th scope="col">Cliente</th>
-                <th scope="col">Assunto</th>
-                <th scope="col">Status</th>
-                <th scope="col">Cadastrado em</th>
-                <th scope="col">#</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td data-label="Cliente">Mercado</td>
-                <td data-label="Assunto">Suporte</td>
-                <td data-label="Status">
-                  <span className="badge" style={{ backgroundColor: "#999" }}>Em aberto</span>
-                </td>
-                <td data-label="Cadastrado">12/02/2022</td>
-                <td data-label="#">
-                  <button className="action" style={{ backgroundColor: "#3583f6" }}>
-                    <FiSearch color="#FFF" size={17} />
-                  </button>
-                  <button className="action" style={{ backgroundColor: "#f6a935" }}>
-                    <FiEdit2 color="#FFF" size={17} />
-                  </button>
-                </td>
-              </tr>
+              <table>
+                <thead>
+                  <tr>
+                    <th scope="col">Cliente</th>
+                    <th scope="col">Assunto</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Cadastrado em</th>
+                    <th scope="col">#</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td data-label="Cliente">Mercado</td>
+                    <td data-label="Assunto">Suporte</td>
+                    <td data-label="Status">
+                      <span className="badge" style={{ backgroundColor: "#999" }}>Em aberto</span>
+                    </td>
+                    <td data-label="Cadastrado">12/02/2022</td>
+                    <td data-label="#">
+                      <button className="action" style={{ backgroundColor: "#3583f6" }}>
+                        <FiSearch color="#FFF" size={17} />
+                      </button>
+                      <button className="action" style={{ backgroundColor: "#f6a935" }}>
+                        <FiEdit2 color="#FFF" size={17} />
+                      </button>
+                    </td>
+                  </tr>
 
+                </tbody>
+              </table>
+            </>
 
-              <tr>
-                <td data-label="Cliente">Informática Tech</td>
-                <td data-label="Assunto">Suporte</td>
-                <td data-label="Status">
-                  <span className="badge" style={{ backgroundColor: "#999" }}>Em aberto</span>
-                </td>
-                <td data-label="Cadastrado">12/02/2022</td>
-                <td data-label="#">
-                  <button className="action" style={{ backgroundColor: "#3583f6" }}>
-                    <FiSearch color="#FFF" size={17} />
-                  </button>
-                  <button className="action" style={{ backgroundColor: "#f6a935" }}>
-                    <FiEdit2 color="#FFF" size={17} />
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          )}
+
         </>
 
       </div>
